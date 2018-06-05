a419a5402a4669b90f9d84d1f88d50a67a1d7608
Chen Boly <chenboly@gmail.com>
2018-06-05 15:58:09 +0700

    Initial Commit

--- src/org/kh/HelloWorld.java
+++ src/org/kh/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World of Java J2SE Class");
+        System.out.println("Hello World");
     }
 }
